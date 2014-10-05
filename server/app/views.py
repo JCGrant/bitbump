da968414a099cf180c04c9099c069eed81604b0d
James Grant <jamescolin.grant@gmail.com>
2014-10-05 06:08:10 +0100

    Removed all slashes from urls

--- server/app/views.py
+++ server/app/views.py
@@ -3,7 +3,7 @@ from flask import request
 from models import User
 from twilio_ import send_message
 
-@app.route('/transaction/', methods=['POST'])
+@app.route('/transaction', methods=['POST'])
 def do_transaction():
     sender_id = request.form['sender']
     receiver_id = request.form['receiver']
@@ -21,7 +21,7 @@ def do_transaction():
         amount, sender.first_name
     ))
 
-@app.route('/users/', methods=["POST"])
+@app.route('/users', methods=["POST"])
 def create_new_user():
     first_name = request.form['first_name']
     last_name = request.form['last_name']
@@ -30,7 +30,7 @@ def create_new_user():
 
     User.create(first_name, last_name, email, phone_number)
 
-@app.route('/balance/')
+@app.route('/balance')
 def get_balance():
     user_id = request.form['user_id']
     user = User.get(user_id)
